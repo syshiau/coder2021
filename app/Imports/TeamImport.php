1d5c719a5cbea3947b5f6ca2dc44fa033c7a6a20
林士立 <t301000@gmail.com>
2021-09-22 20:00:22 +0800

    Update TeamImport.php
    取得 user_id

--- app/Imports/TeamImport.php
+++ app/Imports/TeamImport.php
@@ -3,11 +3,19 @@
 namespace App\Imports;
 
 use App\Models\Team;
+use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class TeamImport implements ToModel, WithHeadingRow
 {
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = User::all(['id', 'username'])->pluck('id', 'username');
+    }
+
     /**
     * @param array $row
     *
@@ -16,7 +24,7 @@ class TeamImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         return new Team([
-            'user_id' => 1,
+            'user_id' => $this->users[$row['帳號']],
             'school' => $row['校名'],
             'game_type' => $row['參賽類別'],
             'player1' => $row['選手1'],
