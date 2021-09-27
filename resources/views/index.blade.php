6b9ca074d17fd574503bee50e808fd060803d31e
林士立 <t301000@gmail.com>
2021-09-27 21:01:12 +0800

    Update index.blade.php
    顯示參賽資料

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,15 @@
         <ul>
             <li>帳號：{{auth()->user()->username}}</li>
         </ul>
+        
+        @if($team = auth()->user()->team)
         <ul>
-            <li>參賽類別：</li>
-            <li>學校：</li>
-            <li>選手1：</li>
-            <li>選手2：</li>
+            <li>參賽類別： {{ $team->game_type }}</li>
+            <li>學校： {{ $team->school }}</li>
+            <li>選手1： {{ $team->player1 }}</li>
+            <li>選手2： {{ $team->player2 }}</li>
         </ul>
+        @endif
     </div>
     @else
      <div>請先登入</div>
